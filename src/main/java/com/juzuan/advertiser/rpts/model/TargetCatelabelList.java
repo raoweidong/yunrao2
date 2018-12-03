64f8d791611c80c58fb20763f86841c79ceff6ee
raoweidong <369708425@qq.com>
2018-12-03 19:01:52 +0800

    ashjfjk

---
+++ src/main/java/com/juzuan/advertiser/rpts/model/TargetCatelabelList.java
@@ -0,0 +1,23 @@
+package com.juzuan.advertiser.rpts.model;
+
+public class TargetCatelabelList {
+    private Integer cateId;
+
+    private String cateName;
+
+    public Integer getCateId() {
+        return cateId;
+    }
+
+    public void setCateId(Integer cateId) {
+        this.cateId = cateId;
+    }
+
+    public String getCateName() {
+        return cateName;
+    }
+
+    public void setCateName(String cateName) {
+        this.cateName = cateName == null ? null : cateName.trim();
+    }
+}
