be3ed5b9487d08490611a6c597880da085edc004
raoweidong <369708425@qq.com>
2018-12-03 17:16:25 +0800

    ashjfjk

--- src/main/java/com/juzuan/advertiser/rpts/mapper/CreativeListMapper.java
+++ src/main/java/com/juzuan/advertiser/rpts/mapper/CreativeListMapper.java
@@ -1,7 +1,9 @@
 package com.juzuan.advertiser.rpts.mapper;
 
 import com.juzuan.advertiser.rpts.model.CreativeList;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface CreativeListMapper {
     int deleteByPrimaryKey(Integer id);
 
