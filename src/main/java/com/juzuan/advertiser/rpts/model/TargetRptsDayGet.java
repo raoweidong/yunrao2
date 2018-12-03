ccc64e8860f63aab9e5b86dbf8225586b752b0eb
raoweidong <369708425@qq.com>
2018-12-03 19:24:44 +0800

    ashjfjk

---
+++ src/main/java/com/juzuan/advertiser/rpts/model/TargetRptsDayGet.java
@@ -0,0 +1,375 @@
+package com.juzuan.advertiser.rpts.model;
+
+import java.util.Date;
+
+public class TargetRptsDayGet {
+    private Integer id;
+
+    private String taobaoUserId;
+
+    private Long campaignId;
+
+    private String campaignName;
+
+    private Long adgroupId;
+
+    private String adgroupName;
+
+    private Long targetId;
+
+    private String targetName;
+
+    private String cvr;
+
+    private String alipayInshopAmt;
+
+    private String alipayInShopNum;
+
+    private String gmvInshopAmt;
+
+    private String gmvInshopNum;
+
+    private String cartNum;
+
+    private String dirShopColNum;
+
+    private String inshopItemColNum;
+
+    private String avgAccessPageNum;
+
+    private String avgAccessTime;
+
+    private String deepInshopUv;
+
+    private String uv;
+
+    private String ecpm;
+
+    private String ecpc;
+
+    private String ctr;
+
+    private String charge;
+
+    private String click;
+
+    private String adPv;
+
+    private String roi;
+
+    private String commodityPurchaseRate;
+
+    private String commodityCollectionRate;
+
+    private String totalCollectionRate;
+
+    private String commodityPlusCost;
+
+    private String commodityCollectionCost;
+
+    private String totalCollectionPlusCost;
+
+    private String averageCostOfOrder;
+
+    private String orderAverageAmount;
+
+    private String averageUvValue;
+
+    private Date logDate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTaobaoUserId() {
+        return taobaoUserId;
+    }
+
+    public void setTaobaoUserId(String taobaoUserId) {
+        this.taobaoUserId = taobaoUserId == null ? null : taobaoUserId.trim();
+    }
+
+    public Long getCampaignId() {
+        return campaignId;
+    }
+
+    public void setCampaignId(Long campaignId) {
+        this.campaignId = campaignId;
+    }
+
+    public String getCampaignName() {
+        return campaignName;
+    }
+
+    public void setCampaignName(String campaignName) {
+        this.campaignName = campaignName == null ? null : campaignName.trim();
+    }
+
+    public Long getAdgroupId() {
+        return adgroupId;
+    }
+
+    public void setAdgroupId(Long adgroupId) {
+        this.adgroupId = adgroupId;
+    }
+
+    public String getAdgroupName() {
+        return adgroupName;
+    }
+
+    public void setAdgroupName(String adgroupName) {
+        this.adgroupName = adgroupName == null ? null : adgroupName.trim();
+    }
+
+    public Long getTargetId() {
+        return targetId;
+    }
+
+    public void setTargetId(Long targetId) {
+        this.targetId = targetId;
+    }
+
+    public String getTargetName() {
+        return targetName;
+    }
+
+    public void setTargetName(String targetName) {
+        this.targetName = targetName == null ? null : targetName.trim();
+    }
+
+    public String getCvr() {
+        return cvr;
+    }
+
+    public void setCvr(String cvr) {
+        this.cvr = cvr == null ? null : cvr.trim();
+    }
+
+    public String getAlipayInshopAmt() {
+        return alipayInshopAmt;
+    }
+
+    public void setAlipayInshopAmt(String alipayInshopAmt) {
+        this.alipayInshopAmt = alipayInshopAmt == null ? null : alipayInshopAmt.trim();
+    }
+
+    public String getAlipayInShopNum() {
+        return alipayInShopNum;
+    }
+
+    public void setAlipayInShopNum(String alipayInShopNum) {
+        this.alipayInShopNum = alipayInShopNum == null ? null : alipayInShopNum.trim();
+    }
+
+    public String getGmvInshopAmt() {
+        return gmvInshopAmt;
+    }
+
+    public void setGmvInshopAmt(String gmvInshopAmt) {
+        this.gmvInshopAmt = gmvInshopAmt == null ? null : gmvInshopAmt.trim();
+    }
+
+    public String getGmvInshopNum() {
+        return gmvInshopNum;
+    }
+
+    public void setGmvInshopNum(String gmvInshopNum) {
+        this.gmvInshopNum = gmvInshopNum == null ? null : gmvInshopNum.trim();
+    }
+
+    public String getCartNum() {
+        return cartNum;
+    }
+
+    public void setCartNum(String cartNum) {
+        this.cartNum = cartNum == null ? null : cartNum.trim();
+    }
+
+    public String getDirShopColNum() {
+        return dirShopColNum;
+    }
+
+    public void setDirShopColNum(String dirShopColNum) {
+        this.dirShopColNum = dirShopColNum == null ? null : dirShopColNum.trim();
+    }
+
+    public String getInshopItemColNum() {
+        return inshopItemColNum;
+    }
+
+    public void setInshopItemColNum(String inshopItemColNum) {
+        this.inshopItemColNum = inshopItemColNum == null ? null : inshopItemColNum.trim();
+    }
+
+    public String getAvgAccessPageNum() {
+        return avgAccessPageNum;
+    }
+
+    public void setAvgAccessPageNum(String avgAccessPageNum) {
+        this.avgAccessPageNum = avgAccessPageNum == null ? null : avgAccessPageNum.trim();
+    }
+
+    public String getAvgAccessTime() {
+        return avgAccessTime;
+    }
+
+    public void setAvgAccessTime(String avgAccessTime) {
+        this.avgAccessTime = avgAccessTime == null ? null : avgAccessTime.trim();
+    }
+
+    public String getDeepInshopUv() {
+        return deepInshopUv;
+    }
+
+    public void setDeepInshopUv(String deepInshopUv) {
+        this.deepInshopUv = deepInshopUv == null ? null : deepInshopUv.trim();
+    }
+
+    public String getUv() {
+        return uv;
+    }
+
+    public void setUv(String uv) {
+        this.uv = uv == null ? null : uv.trim();
+    }
+
+    public String getEcpm() {
+        return ecpm;
+    }
+
+    public void setEcpm(String ecpm) {
+        this.ecpm = ecpm == null ? null : ecpm.trim();
+    }
+
+    public String getEcpc() {
+        return ecpc;
+    }
+
+    public void setEcpc(String ecpc) {
+        this.ecpc = ecpc == null ? null : ecpc.trim();
+    }
+
+    public String getCtr() {
+        return ctr;
+    }
+
+    public void setCtr(String ctr) {
+        this.ctr = ctr == null ? null : ctr.trim();
+    }
+
+    public String getCharge() {
+        return charge;
+    }
+
+    public void setCharge(String charge) {
+        this.charge = charge == null ? null : charge.trim();
+    }
+
+    public String getClick() {
+        return click;
+    }
+
+    public void setClick(String click) {
+        this.click = click == null ? null : click.trim();
+    }
+
+    public String getAdPv() {
+        return adPv;
+    }
+
+    public void setAdPv(String adPv) {
+        this.adPv = adPv == null ? null : adPv.trim();
+    }
+
+    public String getRoi() {
+        return roi;
+    }
+
+    public void setRoi(String roi) {
+        this.roi = roi == null ? null : roi.trim();
+    }
+
+    public String getCommodityPurchaseRate() {
+        return commodityPurchaseRate;
+    }
+
+    public void setCommodityPurchaseRate(String commodityPurchaseRate) {
+        this.commodityPurchaseRate = commodityPurchaseRate == null ? null : commodityPurchaseRate.trim();
+    }
+
+    public String getCommodityCollectionRate() {
+        return commodityCollectionRate;
+    }
+
+    public void setCommodityCollectionRate(String commodityCollectionRate) {
+        this.commodityCollectionRate = commodityCollectionRate == null ? null : commodityCollectionRate.trim();
+    }
+
+    public String getTotalCollectionRate() {
+        return totalCollectionRate;
+    }
+
+    public void setTotalCollectionRate(String totalCollectionRate) {
+        this.totalCollectionRate = totalCollectionRate == null ? null : totalCollectionRate.trim();
+    }
+
+    public String getCommodityPlusCost() {
+        return commodityPlusCost;
+    }
+
+    public void setCommodityPlusCost(String commodityPlusCost) {
+        this.commodityPlusCost = commodityPlusCost == null ? null : commodityPlusCost.trim();
+    }
+
+    public String getCommodityCollectionCost() {
+        return commodityCollectionCost;
+    }
+
+    public void setCommodityCollectionCost(String commodityCollectionCost) {
+        this.commodityCollectionCost = commodityCollectionCost == null ? null : commodityCollectionCost.trim();
+    }
+
+    public String getTotalCollectionPlusCost() {
+        return totalCollectionPlusCost;
+    }
+
+    public void setTotalCollectionPlusCost(String totalCollectionPlusCost) {
+        this.totalCollectionPlusCost = totalCollectionPlusCost == null ? null : totalCollectionPlusCost.trim();
+    }
+
+    public String getAverageCostOfOrder() {
+        return averageCostOfOrder;
+    }
+
+    public void setAverageCostOfOrder(String averageCostOfOrder) {
+        this.averageCostOfOrder = averageCostOfOrder == null ? null : averageCostOfOrder.trim();
+    }
+
+    public String getOrderAverageAmount() {
+        return orderAverageAmount;
+    }
+
+    public void setOrderAverageAmount(String orderAverageAmount) {
+        this.orderAverageAmount = orderAverageAmount == null ? null : orderAverageAmount.trim();
+    }
+
+    public String getAverageUvValue() {
+        return averageUvValue;
+    }
+
+    public void setAverageUvValue(String averageUvValue) {
+        this.averageUvValue = averageUvValue == null ? null : averageUvValue.trim();
+    }
+
+    public Date getLogDate() {
+        return logDate;
+    }
+
+    public void setLogDate(Date logDate) {
+        this.logDate = logDate;
+    }
+}
