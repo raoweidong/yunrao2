0eedf8e897d95991eac19cfcbdb29a709e325165
raoweidong <369708425@qq.com>
2018-12-03 19:29:11 +0800

    ashjfjk

--- src/main/java/com/juzuan/advertiser/rpts/mapper/CrowdListMapper.java
+++ src/main/java/com/juzuan/advertiser/rpts/mapper/CrowdListMapper.java
@@ -3,6 +3,8 @@ package com.juzuan.advertiser.rpts.mapper;
 import com.juzuan.advertiser.rpts.model.CrowdList;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface CrowdListMapper {
     int deleteByPrimaryKey(Integer id);
@@ -16,4 +18,5 @@ public interface CrowdListMapper {
     int updateByPrimaryKeySelective(CrowdList record);
 
     int updateByPrimaryKey(CrowdList record);
+    List<CrowdList> selectAllCrowd();
 }
