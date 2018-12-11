fa40926b6adf8acc475df61d7427a8de7e3ab8b6
raoweidong <369708425@qq.com>
2018-12-11 09:48:21 +0800

    ashjfjk

--- src/main/java/com/juzuan/advertiser/rpts/controller/AdzoneRptsDayGetController.java
+++ src/main/java/com/juzuan/advertiser/rpts/controller/AdzoneRptsDayGetController.java
@@ -2,27 +2,34 @@ package com.juzuan.advertiser.rpts.controller;
 
 import com.juzuan.advertiser.rpts.mapper.AdvertiserAdzoneRptsDayGetMapper;
 import com.juzuan.advertiser.rpts.model.AdvertiserAdzoneRptsDayGet;
+import com.juzuan.advertiser.rpts.model.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-@ResponseBody
+
 @Controller
 public class AdzoneRptsDayGetController {
     @Autowired
     private AdvertiserAdzoneRptsDayGetMapper advertiserAdzoneRptsDayGetMapper;
-    @RequestMapping(value = "/adzonesDate",produces ={"application/json;charset=UTF-8"},method = RequestMethod.POST)
-    public List<AdvertiserAdzoneRptsDayGet> queryAdzoneRptsDayGets(){
-
-        Map<String,Object> data=new HashMap();
-        data.put("currIndex",10);
-        data.put("pageSize",10);
-
-        return advertiserAdzoneRptsDayGetMapper.queryAdzoneRptsDayGetsBySql(data);
+    @ResponseBody
+    @RequestMapping(value = "/adzonesDate",produces ={"application/json;charset=UTF-8"})
+    public Page<AdvertiserAdzoneRptsDayGet> findWithPage(int pageNum, int pageSize){
+        Map<String,Object> data=new HashMap<>();
+        data.put("startIndex",(pageNum-1)*pageSize);
+        data.put("pageSize",pageSize);
+        List<AdvertiserAdzoneRptsDayGet> ss=advertiserAdzoneRptsDayGetMapper.queryAdzoneRptsDayGetsBySql(data);
+        int totalRecord=advertiserAdzoneRptsDayGetMapper.countByNull();
+           Page pb=new Page(pageNum,pageSize,totalRecord);
+        pb.setTotalRecord(totalRecord);
+        pb.setList(ss);
+        System.out.println("哈哈");
+        return pb;
     }
 }
