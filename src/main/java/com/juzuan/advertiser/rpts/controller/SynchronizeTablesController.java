9a176203b21671a3d4d79f1f92d7bb4e3f106896
raoweidong <369708425@qq.com>
2018-12-27 11:06:22 +0800

    ashjfjk

--- src/main/java/com/juzuan/advertiser/rpts/controller/SynchronizeTablesController.java
+++ src/main/java/com/juzuan/advertiser/rpts/controller/SynchronizeTablesController.java
@@ -1,5 +1,8 @@
 package com.juzuan.advertiser.rpts.controller;
 
+import com.juzuan.advertiser.rpts.mapper.TaobaoAuthorizeUserMapper;
+import com.juzuan.advertiser.rpts.model.Response;
+import com.juzuan.advertiser.rpts.model.TaobaoAuthorizeUser;
 import com.juzuan.advertiser.rpts.service.SynchronizeTablesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,10 +15,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class SynchronizeTablesController {
     @Autowired
     private SynchronizeTablesService synchronizeTablesService;
+    @Autowired
+    private TaobaoAuthorizeUserMapper taobaoAuthorizeUserMapper;
     @RequestMapping(value = "tongbuTables")
-    public String tongbuTables(){
-        synchronizeTablesService.synchronize();
-        return "同步成功";
+    public Response tongbuTables(String userId){
+        TaobaoAuthorizeUser taobaoAuthorizeUser=taobaoAuthorizeUserMapper.slectByUserId(userId);
+        Response response=new Response();
+        if (taobaoAuthorizeUser!=null){
+            String  chuShiShuJu= synchronizeTablesService.synchronize(userId);
+            response.setCode(0);
+            response.setMsg("success");
+        }else {
+            response.setCode(1);
+            response.setMsg("非法参数");
+        }
+        return response;
     }
 
 }
